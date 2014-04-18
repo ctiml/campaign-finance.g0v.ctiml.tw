cb354e857fd42ebf178b22b174ca9a876c1ef6ca
Timothy Lee <timmy.ccl@gmail.com>
2014-04-19 00:46:14 +0800

    調整程式碼，加上 enter 送出功能

--- static/js/cell.js
+++ static/js/cell.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
-  $('#submit').on('click', function(e){
-    e.preventDefault();
-    var data = {
-      page: $('.cell-info').data('page'),
-      x: $('.cell-info').data('x'),
-      y: $('.cell-info').data('y'),
-      ans: $('#ans').val()
-    };
-    var url = '/api/fillcell/' + data.page + "/" + data.x + "/" + data.y;
-    $.post(url, {ans: data.ans}, function(){
-      $('.msg').text('ok');
-    });
-  });  
 
-  $('#reset').on('click', function(e){
+  var submitAnswer = function(e) {
+    if (e !== undefined) {
+      e.preventDefault();
+    }
+
+    var ans = $('#ans').val();
+    if (ans === "") {
+      return;
+    }
+    var page = $('.cell-info').data('page');
+    var x = $('.cell-info').data('x');
+    var y = $('.cell-info').data('y');
+
+    $('#submit').attr('disabled', 'disabled');
+
+    var url = ['/api/fillcell/', page, "/", x, "/", y].join("");
+    $.post(url, { ans: ans }, function(res){
+      window.location.reload();
+    });
+  };
+
+  $('#submit').click(submitAnswer);
+
+  $('#next').click(function(e) {
     window.location.reload();
-  });  
+  });
+
+  $('#ans').keypress(function(e) {
+    if (e.which == 13) {
+      e.preventDefault();
+      submitAnswer();
+    }
+  }).focus();
 
 });
