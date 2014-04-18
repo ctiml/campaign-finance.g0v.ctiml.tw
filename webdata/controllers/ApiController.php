8aa45d82c8b9e3a13388afc8e5083b21eab48af6
Timothy Lee <timmy.ccl@gmail.com>
2014-04-19 00:50:46 +0800

    取得 cell 值的 api

--- webdata/controllers/ApiController.php
+++ webdata/controllers/ApiController.php
@@ -21,4 +21,20 @@ class ApiController extends Pix_Controller
         }
         return $this->noview();
     }
+
+    public function getcellvalueAction()
+    {
+        list(, /*api*/, /*getcellvalue*/, $page, $x, $y) = explode('/', $this->getURI());
+        $page = intval($page);
+        $x = intval($x);
+        $y = intval($y);
+
+        $values = array('page' => $page, 'x' => $x, 'y' => $y);
+        $cell = Cell::search($values)->first();
+        if ($cell == NULL) {
+            return $this->json(array('error' => true, 'message' => 'not found'));
+        } else {
+            return $this->json(array('error' => false, 'value' => $cell->ans));
+        }
+    }
 }
