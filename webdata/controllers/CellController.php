4f400c6b07421132f4268bbb6af6acd238b9c065
Timothy Lee <timmy.ccl@gmail.com>
2014-04-19 01:27:42 +0800

    如果已經有人填寫了該欄位就顯示出來

--- webdata/controllers/CellController.php
+++ webdata/controllers/CellController.php
@@ -4,9 +4,15 @@ class CellController extends Pix_Controller
 {
     public function indexAction()
     {
-        $page = rand(1, 2000);
+        $page = rand(1, 2500);
         $x = rand(2, 21);
         $y = rand(1, 9);
+
+        $cell = Cell::search(array('page' => $page, 'x' => $x, 'y' => $y))->first();
+        if ($cell != NULL) {
+            $this->view->cell = $cell;
+        }
+
         $this->view->cellimg = "http://campaign-finance.g0v.ronny.tw/api/getcellimage/" . $page . "/" . $x . "/" . $y . ".png";
         $this->view->page = $page;
         $this->view->x = $x;
