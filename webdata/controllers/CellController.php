f0ec0b9726f0d0dcb2d08a1243d46fc5171d2f3d
Timothy Lee <timmy.ccl@gmail.com>
2014-04-20 23:35:35 +0800

    移除沒用到的程式碼

--- webdata/controllers/CellController.php
+++ webdata/controllers/CellController.php
@@ -5,9 +5,4 @@ class CellController extends Pix_Controller
     public function indexAction()
     {
     }
-
-    public function fillAction()
-    {
-        $this->view->cellimg = "http://" . strval(getenv(CAMPAIGN_FINANCE_RONNY)) . "/api/getcellimage/775/2/1.png";
-    }
 }
